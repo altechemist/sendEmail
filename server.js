a9179c9831a9bbc0287b418cd48609a2839ed36c
Karabelo Nthoroane <karabelonthoroane@gmail.com>
2025-01-24 08:28:46 +0200

    Contact form endpoint

--- server.js
+++ server.js
@@ -61,7 +61,31 @@ async function sendConfirmationEmail(
   }
 }
 
-// Room reservation endpoint
+// Helper function to send contact email
+async function sendContactEmail(name, email, message) {
+  try {
+    const mailOptions = {
+      from: email, // sender address
+      to: senderEmail, // recipient address
+      subject: "Portfolio Message", // Subject line
+      text: message,
+      html:
+        `<b>Hello ${name},</b><br><br>` +
+        `Your room reservation has been confirmed with the following details:<br><br>` +
+        `<b>Name:</b> ${name}<br><b>Email:</b> ${email}<br><b>Room Type:</b> ${roomType}<br>` +
+        `<b>Check-in Date:</b> ${checkInDate}<br><b>Check-out Date:</b> ${checkOutDate}<br><br>` +
+        `Thank you for choosing us!<br><br>Best regards,<br>Luxe Hotel`,
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log("Message sent: %s", info.messageId);
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+}
+
+
+      // Room reservation endpoint
 app.post("/send-email", async (req, res) => {
   const { name, email, roomType, checkInDate, checkOutDate } = req.body;
 
@@ -86,7 +110,38 @@ app.post("/send-email", async (req, res) => {
   });
 });
 
-// Start the server
+// Send contact form email endpoint
+app.post("/send-contact-email", async (req, res) => {
+  const { name, email, message } = req.body;
+
+  // Basic validation
+  if (!name || !email || !message) {
+    return res.status(400).json({ error: "Missing required fields" });
+  }
+
+  try {
+    const mailOptions = {};
+    // Send the contact email
+    await sendContactEmail(name, email, message);
+
+    // Respond with a success message
+    res.status(200).json({
+      message: `Contact email sent from ${name}.`,
+      contact: {
+        name,
+        email,
+        message,
+      },
+    });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    res.status(500).json({ error: "An error occurred while sending the email" });
+  }
+});
+
+
+
+      // Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
